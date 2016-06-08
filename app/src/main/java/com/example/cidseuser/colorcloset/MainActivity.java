9f3aa032d41b59c7a9b3df26354211fd0450b1dd
cidseuser <marksushifoot@gmail.com>
2016-06-08 12:56:38 -0700

    Here I have added code for color detection

--- app/src/main/java/com/example/cidseuser/colorcloset/MainActivity.java
+++ app/src/main/java/com/example/cidseuser/colorcloset/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.cidseuser.colorcloset;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Bitmap bMap = BitmapFactory.decodeResource(getResources(), R.drawable.shirt_test);
+
+        ColorDetection colorDet = new ColorDetection();
+        int x = colorDet.getDominantColor(bMap);
+
+        Log.i("MainActivity","Value -= " + x);
+
     }
 }
