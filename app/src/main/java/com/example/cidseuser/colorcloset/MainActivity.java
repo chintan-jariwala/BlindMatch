65869579017f5a9b42034c8a03b51548bd0cc55c
ManalotoM <marksushifoot@gmail.com>
2016-06-10 12:38:41 -0700

    Update MainActivity.java

--- app/src/main/java/com/example/cidseuser/colorcloset/MainActivity.java
+++ app/src/main/java/com/example/cidseuser/colorcloset/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.cidseuser.colorcloset;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.*;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
