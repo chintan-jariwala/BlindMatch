09008a66381a7ebeb45edf262fbde6a5fe5bf846
ManalotoM <marksushifoot@gmail.com>
2016-06-10 12:39:41 -0700

    Update MainActivity.java

--- app/src/main/java/com/example/cidseuser/colorcloset/MainActivity.java
+++ app/src/main/java/com/example/cidseuser/colorcloset/MainActivity.java
@@ -16,3 +16,65 @@ public class MainActivity extends AppCompatActivity {
 
 
 }
+
+
+package com.example.cidseuser.colorcloset;
+import java.util.ArrayList;
+import java.util.*;
+
+/**
+ * Created by cidseuser on 6/10/2016.
+ */
+public class Clothing {
+
+    private String hexvalue;
+
+
+    public class shirt extends Clothing {
+        public shirt(String hvalue){
+
+        }
+    }
+
+    public class cShirt extends Clothing {
+        public cShirt(String hvalue){
+
+        }
+    }
+
+    public class sweater extends Clothing {
+        public sweater(String hvalue){
+
+        }
+
+    }
+
+    public class jacket extends Clothing {
+        public jacket(String hvalue){
+
+        }
+
+    }
+
+    public class bottom extends Clothing {
+        public bottom(String hvalue){
+
+        }
+
+    }
+
+    public class other extends Clothing {
+        public other(String hvalue){
+
+        }
+
+    }
+
+    public class dress extends Clothing {
+        public dress(String hvalue){
+
+        }
+
+    }
+
+}
