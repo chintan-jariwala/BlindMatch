121ba72939c776d3d1cd23b9b171a9b79c33658b
ManalotoM <marksushifoot@gmail.com>
2016-06-09 23:33:31 -0700

    Update ColorDetection.java

--- app/src/main/java/com/example/cidseuser/colorcloset/ColorDetection.java
+++ app/src/main/java/com/example/cidseuser/colorcloset/ColorDetection.java
@@ -51,13 +51,17 @@ public class ColorDetection {
     }
 
     public String getMostCommonColor(Map map){
+        //todo: fix this guy
 
         List list = new LinkedList(map.entrySet());
         Collections.sort(list, new Comparator() {
-            @Override
             public int compare(Object o1, Object o2) {
-                return ((Comparable)((Map.Entry)(o1)).getValue().compareTo(((Map.Entry)(o2)).getValue());
+
+                return ((Comparable) ((Map.Entry) (o1)).getValue())
+                        .compareTo(((Map.Entry) (o2)).getValue());
+
             }
+
         });
 
         Map.Entry me = (Map.Entry)list.get(list.size()-1);
@@ -107,19 +111,15 @@ public class ColorDetection {
     //This actually retrieves the average rgb color
 
     /*public int getDominantColor(Bitmap bitmap){
-
         if (null == bitmap) return Color.TRANSPARENT;
-
         int redBucket = 0;
         int greenBucket = 0;
         int blueBucket = 0;
         int alphaBucket = 0;
-
         boolean hasAlpha = bitmap.hasAlpha();
         int pixelCount = bitmap.getWidth() * bitmap.getHeight();
         int[] pixels = new int[pixelCount];
         bitmap.getPixels(pixels, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
-
         for (int y = 0, h = bitmap.getHeight(); y < h; y++)
         {
             for (int x = 0, w = bitmap.getWidth(); x < w; x++)
@@ -131,13 +131,11 @@ public class ColorDetection {
                 if (hasAlpha) alphaBucket += (color >>> 24); // Color.alpha
             }
         }
-
         return Color.argb(
                 (hasAlpha) ? (alphaBucket / pixelCount) : 255,
                 redBucket / pixelCount,
                 greenBucket / pixelCount,
                 blueBucket / pixelCount);
-
     }*/
 
     //This retrieves the dominant rbg value in an image
