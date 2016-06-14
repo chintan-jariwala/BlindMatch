9b8c826de6ef7d488f6f96c2873330bd06dfccf9
ManalotoM <marksushifoot@gmail.com>
2016-06-13 23:40:57 -0700

    Update Outfit.java

--- app/src/main/java/com/example/cidseuser/colorcloset/Outfit.java
+++ app/src/main/java/com/example/cidseuser/colorcloset/Outfit.java
@@ -1,20 +1,70 @@
-package com.example.cidseuser.colorcloset;
+package com.example.mark.myapplication;
 import android.graphics.Color;
-import java.util.ArrayList;
+
+import java.lang.reflect.Array;
+import java.util.List;
 
 /**
  * Created by cidseuser on 6/13/2016.
  */
 public class Outfit {
 
-    public filter{
+    public static int getComplementaryColor(int colorToInvert) {
+        float[] hsv = new float[3];
+        Color.RGBToHSV(Color.red(colorToInvert), Color.green(colorToInvert),
+                Color.blue(colorToInvert), hsv);
+        hsv[0] = (hsv[0] + 180) % 360;
 
+        //TODO: narrow this color down to just primary colors (or limited set of colors)
+        return Color.HSVToColor(hsv);
+    }
+    public List<Clothing> Filter(List<Clothing> clothes){
+        // get complement of either top or bottom (whichever is not null)
+        // create a new list of clothes filtered based on the complementary color
+        // return this new list
+        List<Clothing> result = null;
+        if(top != null){
+            int comp = getComplementaryColor(top.getColor());
+            //todo: log value of comp
+
+            // convert complementary color to a simple color
+            comp = ColorHelper.getBestMatchingColorName(comp);
+
+            for(Clothing c : clothes){
+                if(c.getColor() == comp)
+                    result.add((Clothing) c);
+            }
+        } else {
+            int comp = getComplementaryColor(bottom.getColor());
+            //todo: log value of comp
+
+            for(Clothing c : clothes){
+                if(c.getColor() == comp)
+                    result.add((Clothing) c);
+            }
+        }
+
+
+        //todo: log result.count
+        return result;
     }
 
-    public setTops{
+    private Top top;
+    private Bottom bottom;
+
+    public void setTop(Top top){
+        this.top = top;
     }
 
-    public setBottoms{
+    public Top getTop(){
+        return top;
+    }
 
+    public void setBottom(Bottom bottom){
+        this.bottom = bottom;
+    }
+
+    public Bottom GetBottom(){
+        return bottom;
     }
 }
