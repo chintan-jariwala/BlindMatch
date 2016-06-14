2ce9c2258c2f72ad838bc334c3d0f54533c32486
ManalotoM <marksushifoot@gmail.com>
2016-06-13 23:41:17 -0700

    Create Top.java

---
+++ app/src/main/java/com/example/cidseuser/colorcloset/Top.java
@@ -0,0 +1,10 @@
+package com.example.mark.myapplication;
+
+/**
+ * Created by Mark on 6/13/2016.
+ */
+public class Top extends Clothing {
+    public Top(int rgb, String[] tags, String description) {
+        super(rgb, tags, description);
+    }
+}
