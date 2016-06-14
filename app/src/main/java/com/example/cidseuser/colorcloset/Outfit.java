cb2539df9a203696e76e17101e0f8eaa37151528
ManalotoM <marksushifoot@gmail.com>
2016-06-13 18:27:23 -0700

    Update and rename Color.java to Outfit.java

---
+++ app/src/main/java/com/example/cidseuser/colorcloset/Outfit.java
@@ -0,0 +1,20 @@
+package com.example.cidseuser.colorcloset;
+import android.graphics.Color;
+import java.util.ArrayList;
+
+/**
+ * Created by cidseuser on 6/13/2016.
+ */
+public class Outfit {
+
+    public filter{
+
+    }
+
+    public setTops{
+    }
+
+    public setBottoms{
+
+    }
+}
