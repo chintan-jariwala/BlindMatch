2309d7c5225ec8de31315cdfd9526b676755655f
ManalotoM <marksushifoot@gmail.com>
2016-06-13 18:28:45 -0700

    Update and rename getComplementaryColor.java to Wardrobe.java

---
+++ app/src/main/java/com/example/cidseuser/colorcloset/Wardrobe.java
@@ -0,0 +1,26 @@
+package com.example.cidseuser.colorcloset;
+import android.graphics.Color;
+
+import java.util.ArrayList;
+
+/**
+ * Created by cidseuser on 6/13/2016.
+ */
+public class Wardrobe {
+
+    public Wardrobe(List<Clothing> c) {
+        clothes = c;
+    }
+
+    privste ArrayList<Clothing> clothes;
+
+    public ArrayList<Clothing> getTops(){
+        // todo: return only tops
+        return c;
+    }
+
+    public ArrayList<Clothing> getBottoms(){
+        // todo: return only bottoms
+        return c;
+    }
+}
