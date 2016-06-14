76f18916525a946ce5263839b7bb872a4af0a530
ManalotoM <marksushifoot@gmail.com>
2016-06-13 23:39:00 -0700

    Create Bottom.java

---
+++ app/src/main/java/com/example/cidseuser/colorcloset/Bottom.java
@@ -0,0 +1,10 @@
+package com.example.mark.myapplication;
+
+/**
+ * Created by Mark on 6/13/2016.
+ */
+public class Bottom extends Clothing {
+    public Bottom(int rgb, String[] tags, String description) {
+        super(rgb, tags, description);
+    }
+}
