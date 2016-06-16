72a068acae5bc0bfcfb90b7defd344a7121be49f
ManalotoM <marksushifoot@gmail.com>
2016-06-16 09:41:51 -0700

    About to get the color detection and complementary color working Garistyn will put code behind UI

--- app/src/main/java/com/example/cidseuser/colorcloset/Camera2BasicFragment.java
+++ app/src/main/java/com/example/cidseuser/colorcloset/Camera2BasicFragment.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+//jij
 package com.example.cidseuser.colorcloset;
 
 import android.Manifest;
