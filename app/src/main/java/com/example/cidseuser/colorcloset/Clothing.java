31aec13980f7734f1135ae2ed6b39cda2d755c6d
ManalotoM <marksushifoot@gmail.com>
2016-11-05 11:51:38 -0700

    Update Clothing.java

--- app/src/main/java/com/example/cidseuser/colorcloset/Clothing.java
+++ app/src/main/java/com/example/cidseuser/colorcloset/Clothing.java
@@ -1,8 +1,13 @@
 package com.example.cidseuser.colorcloset;
 import android.graphics.Color;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.List;
 import java.lang.*;
+import java.util.UUID;
+
 /**
  * Created by cidseuser on 6/14/2016.
  */
@@ -11,13 +16,42 @@ public class Clothing {
         color = rgb;
     }
 
+    private static final String JSON_ID = "id";
+    private static final String JSON_COLOR = "color";
+    private static final String JSON_PHOTOFILEPATH = "photofilepath";
+
     private List<Top> tops;
     private List<Bottom> bottoms;
+    private UUID id;
     private String color;
     private String photofilepath;
-    private int id;
 
-    public String getColor(){ return color; }
-    public String getPhotofilepath(){ return photofilepath; }
-    public int getId(){ return id; }
+    /*private String color;
+    private String photofilepath;
+    private int id;*/
+
+    public String getColor(){
+        return color;
+    }
+
+    public String getPhotofilepath(){
+        return photofilepath;
+    }
+
+    public UUID getId(){
+        return id;
+    }
+
+    public Clothing(){
+        id = UUID.randomUUID();
+    }
+
+    public JSONObject toJSON() throws JSONException{
+        JSONObject json = new JSONObject();
+        json.put(JSON_ID, id.toString());
+        json.put(JSON_COLOR, color.toString());
+        json.put(JSON_PHOTOFILEPATH, photofilepath.toString());
+        return json;
+    }
+
 }
