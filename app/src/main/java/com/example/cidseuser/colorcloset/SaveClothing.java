1be0ec9dd6e2cebbfaa4b8f1b6231a966962e262
ManalotoM <marksushifoot@gmail.com>
2016-11-05 11:50:19 -0700

    Create SaveClothing.java

---
+++ app/src/main/java/com/example/cidseuser/colorcloset/SaveClothing.java
@@ -0,0 +1,13 @@
+package com.example.cidseuser.colorcloset;
+
+/**
+ * Created by temp6435 on 11/5/2016.
+ */
+public class SaveClothing {
+
+    private static final String TAG = "SaveClothing";
+    private static final String FILENAME = "clothing.json";
+
+
+
+}
