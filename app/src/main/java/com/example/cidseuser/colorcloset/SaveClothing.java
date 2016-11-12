fa94a5568f8afa3f6845fafcb6cffb2074aee993
ManalotoM <marksushifoot@gmail.com>
2016-11-11 22:30:38 -0700

    Update SaveClothing.java

--- app/src/main/java/com/example/cidseuser/colorcloset/SaveClothing.java
+++ app/src/main/java/com/example/cidseuser/colorcloset/SaveClothing.java
@@ -1,5 +1,10 @@
 package com.example.cidseuser.colorcloset;
 
+import android.content.Context;
+import android.util.Log;
+
+import java.util.ArrayList;
+
 /**
  * Created by temp6435 on 11/5/2016.
  */
@@ -8,6 +13,40 @@ public class SaveClothing {
     private static final String TAG = "SaveClothing";
     private static final String FILENAME = "clothing.json";
 
+    private ArrayList<Clothing> mClothing;
+    private ClothingJSONSerializer mSerializer;
 
+    private static Clothing sClothing;
+    private Context mAppContext;
+
+    private SaveClothing(Context mAppContext){
+        mAppContext = mAppContext;
+        mClothing = new ArrayList<Clothing>();
+        mSerializer = new ClothingJSONSerializer(mAppContext, FILENAME);
+
+        try{
+            mClothing = mSerializer.loadClothing();
+        }
+        catch (Exception e){
+            mClothing = new ArrayList<>(Clothing);
+            Log.e(TAG, "Error loading clothing: ", e);
+        }
+    }
+
+    public void addClothing(Clothing c){
+        mClothing.add(c);
+    }
+
+    public boolean saveClothing{
+        try{
+            mSerializer.saveClothing(mClothing);
+            Log.d(TAG, "clothing saved to file");
+            return true;
+        }
+        catch (Exception e){
+            Log.e(TAG, "Error saving clothing: ", e);
+            return false;
+        }
+    }
 
 }
