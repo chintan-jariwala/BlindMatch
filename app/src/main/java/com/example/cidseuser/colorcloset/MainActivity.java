63347db005412800210577e2d47bb56519d6ce0a
ManalotoM <marksushifoot@gmail.com>
2016-12-01 00:51:57 -0700

    Still need to get save working

--- app/src/main/java/com/example/cidseuser/colorcloset/MainActivity.java
+++ app/src/main/java/com/example/cidseuser/colorcloset/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.cidseuser.colorcloset;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText DescriptionEditText;
+    TextView textViewDescription;
+    Button SaveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SaveButton = (Button)findViewById(R.id.button);
+        DescriptionEditText = (EditText)findViewById(R.id.editDescription);
+        //SaveButton.setOnClickListener(new View.OnClickListener);
     }
+
+    /*@Override
+    public View onCreateView()
+
+    public void onClick(View view){
+        final String Name = DescriptionEditText.getText().toString();
+    }*/
+
+
 }
